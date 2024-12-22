30de4fba541bc26550b62cf953d0ebef3f36efaf
Anand-Theertha <anandtheerthav3@gmail.com>
2024-12-22 08:39:14 +0000

    Base Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
+import { Routes , Route  } from 'react-router-dom';
+import SignInForm from './_auth/forms/SignInForm';
+import { Home } from './_root/pages';
+import SignUpForm from './_auth/forms/SignUpForm';
+import AuthLayout from './_auth/AuthLayout';
+
+import './globals.css'
+import RootLayout from './_root/RootLayout';
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className='flex h-screen'>
+      <Routes>
+        {/* Public Route */}
+        <Route element={<AuthLayout />}>
+          <Route path='/sign-up' element={<SignUpForm />} />
+          <Route path='/sign-in' element={<SignInForm />} />
+        </Route>
+
+        {/* Private Route */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+
+      </Routes>
+    </div>
   )
 }
 
